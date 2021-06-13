f401abb752a58117dbe2ddfb4a26f961a6230334
Charles Wefso <cwefso@gmail.com>
2021-06-13 08:40:18 -0600

    installs material-ui

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/details">Details</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/details">
+            {/* <Users /> */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
